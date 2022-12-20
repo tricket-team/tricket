904c25d471bb632fa2b370cc1fd66e154b75b518
nata-non <63070045@kmitl.ac.th>
2022-12-20 23:32:09 +0700

    feat : card

--- packages/web/pages/signin.tsx
+++ packages/web/pages/signin.tsx
@@ -18,75 +18,99 @@ function signin() {
   }, [email]);
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-slate-100">
-      <div className="flex flex-col gap-y-3 py-3">
-        <div>
-          <input
-            className="border-2 w-full"
-            type="email"
-            placeholder="Email"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </div>
-
-        <div>
-          <div className="relative">
-            <div className="absolute right-1 flex items-center">
-              {passwordShown ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-7 h-7 cursor-pointer"
-                  onClick={togglePassword}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-7 h-7 cursor-pointer"
-                  onClick={togglePassword}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                  />
-                </svg>
-              )}
+      <div className="w-[40%] h-[50%] rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="flex flex-col gap-y-3 py-2">
+            <div>
+              <p className="text-4xl text-center" style={{ color: '#4EE191' }}>
+                Sign in
+              </p>
             </div>
-            <input
-              className="border-2 w-full"
-              type={passwordShown ? 'text' : 'password'}
-              placeholder="Password"
-            ></input>
+            <div>
+              <label htmlFor="">Email</label>
+              <input
+                className="border-2 w-full p-1 rounded-md"
+                type="email"
+                placeholder="Email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="">Password</label>
+              <div className="relative">
+                <div className="absolute top-1 right-1 flex items-center">
+                  {passwordShown ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-7 h-7 cursor-pointer"
+                      onClick={togglePassword}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-7 h-7 cursor-pointer"
+                      onClick={togglePassword}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  )}
+                </div>
+                <input
+                  className="border-2 w-full p-1 rounded-md"
+                  type={passwordShown ? 'text' : 'password'}
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center px-6 pb-2 items-center">
+          <button
+            style={{ backgroundColor: '#4EE191', height: 45 }}
+            className="rounded-[16px] w-full"
+          >
+            Login
+          </button>
+        </div>
+        <div className="flex px-6 pb-2 items-center">
+          <div className="w-[50%]">
+            <a href="">
+              <p className="">Create Account</p>
+            </a>
+          </div>
+          <div className="w-[50%]">
+            <a href="">
+              <p className="text-end">Forgot Password</p>
+            </a>
           </div>
         </div>
       </div>
-
-      <button
-        style={{ backgroundColor: '#4EE191', width: 107, height: 45 }}
-        className="rounded-[16px]"
-      >
-        Login
-      </button>
     </div>
   );
 }

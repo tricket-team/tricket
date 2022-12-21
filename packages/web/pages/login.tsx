14ffd6632b70934653951d0086e3b3b4f5d0efaa
nata-non <63070045@kmitl.ac.th>
2022-12-21 19:42:37 +0700

    feat : login

---
+++ packages/web/pages/login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Signin from './sign-in';
+function Login() {
+  return (
+    <>
+      <NavBar />
+      <Signin />
+    </>
+  );
+}
+
+export default Login;
--- packages/web/pages/sign-in.tsx
+++ packages/web/pages/sign-in.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-function signin() {
+import NavBar from '../components/NavBar';
+function Signin() {
   const user = [
     {
       id: 1,
@@ -9,6 +10,7 @@ function signin() {
     },
   ];
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [passwordShown, setPasswordShown] = useState(false);
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
@@ -85,6 +87,9 @@ function signin() {
                   className="border-2 w-full p-1 rounded-md"
                   type={passwordShown ? 'text' : 'password'}
                   placeholder="Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
                 ></input>
               </div>
             </div>
@@ -100,14 +105,74 @@ function signin() {
         </div>
         <div className="flex px-6 pb-2 items-center">
           <div className="w-[50%]">
-            <a href="">
-              <p className="">Create Account</p>
+            <a href="/create">
+              <p className="text-green-500">Create Account</p>
             </a>
           </div>
           <div className="w-[50%]">
             <a href="">
-              <p className="text-end">Forgot Password</p>
+              <p className="text-green-500 flex justify-end">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                    />
+                  </svg>
+                </span>
+                Forgot Password
+              </p>
             </a>
+          </div>
+        </div>
+        <div className="flex px-6 pb-2 items-center">
+          <div
+            className="bg-black col-end-10 w-[40%]"
+            style={{ height: 1 }}
+          ></div>
+          <p className="mx-3">or sign in with</p>
+          <div
+            className="bg-black col-end-10 w-[40%]"
+            style={{ height: 1 }}
+          ></div>
+        </div>
+
+        <div className="flex px-6 pb-2 items-center">
+          <div className="flex items-center justify-center w-full">
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="33"
+                height="90"
+                preserveAspectRatio="xMidYMid"
+                viewBox="0 0 256 262"
+              >
+                <path
+                  fill="#4285F4"
+                  d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+                />
+                <path
+                  fill="#34A853"
+                  d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+                />
+                <path
+                  fill="#FBBC05"
+                  d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
+                />
+                <path
+                  fill="#EB4335"
+                  d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
@@ -115,4 +180,4 @@ function signin() {
   );
 }
 
-export default signin;
+export default Signin;

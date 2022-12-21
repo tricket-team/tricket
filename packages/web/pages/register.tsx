652580633b1da7d150c843a0b34f49b1d9b49b3b
nata-non <63070045@kmitl.ac.th>
2022-12-21 21:25:53 +0700

    feat : register

---
+++ packages/web/pages/register.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Signin from './sign-in';
+import Footer from '../components/Footer';
+function Register() {
+  return (
+    <>
+      <NavBar />
+      <Footer />
+    </>
+  );
+}
+
+export default Register;

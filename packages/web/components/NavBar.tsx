ebd98fd49cc3cdbeaccd9f6ef7e79d71eb669cfd
santhitak <63070175@kmitl.ac.th>
2022-12-21 23:54:11 +0700

    feat: coloring on path change

--- packages/web/components/NavBar.tsx
+++ packages/web/components/NavBar.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const NavBar = () => {
+  const router = useRouter();
   return (
     <>
       <div className="fixed w-full grid grid-cols-12 border-2">
         <div className="col-start-2 col-end-12 my-5">
-          <div className="bg-white/[.03]	backdrop-blur-md rounded-lg">
+          <div className="bg-black/[.03]	backdrop-blur-md rounded-lg">
             <div className="py-3 px-5 flex justify-between items-center">
-              <Image src={'/img/tricket.svg'} width={120} height={50} alt="" />
-              <div className="text-[#24475B] font-semibold flex gap-x-10">
+              <Image
+                src={
+                  router.pathname === '/'
+                    ? '/img/tricket.svg'
+                    : '/img/tricket-white.svg'
+                }
+                width={120}
+                height={50}
+                alt=""
+              />
+              <div
+                className={`
+                  ${
+                    router.pathname === '/' ? 'text-[#24475B]' : 'text-white'
+                  } font-semibold flex gap-x-10`}
+              >
                 <p>Home</p>
                 <p>Profile</p>
                 <a href="/login">

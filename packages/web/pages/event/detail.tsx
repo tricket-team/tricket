f5da69bd46e3f096f02e261938eba0e93519347a
santhitak <63070175@kmitl.ac.th>
2022-12-21 17:50:58 +0700

    feat: ticket

--- packages/web/pages/event/detail.tsx
+++ packages/web/pages/event/detail.tsx
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Navbar from '../../components/NavBar';
+import { Menu } from '@headlessui/react';
+import { eventDetailDummy, TicketType } from '../../data';
 
 function Detail() {
+  const [ticketCount, setTicketCount] = useState([]);
+  const [ticketNumber, setTicketNumber] = useState([]);
+
+  const ticketQuantity = (ticket: number) => {
+    for (let i = 0; i < ticket + 1; i++) {
+      ticketNumber.push(i);
+    }
+    return setTicketNumber;
+  };
+
+  useState(() =>
+    eventDetailDummy.ticket.forEach((element) => {
+      ticketCount.push(element);
+      ticketQuantity(element.quantity);
+    })
+  );
+
+  useState(() => console.log(ticketCount));
+
   return (
     <div className="grid grid-cols-12 mb-10">
+      <Navbar />
       <div className="col-span-12">
         <Image
-          src="https://scontent.fbkk31-1.fna.fbcdn.net/v/t39.30808-6/294815604_5222659704488338_2909628041657591105_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=e25c_dmcgSMAX9_vYy9&_nc_ht=scontent.fbkk31-1.fna&oh=00_AfBxu8VuB57M4voGQe3UZkOH1zN6zeIXCm7Sm5AmpFt-cA&oe=63A6CFB7"
+          src={eventDetailDummy.coverImg}
           width={1920}
           height={300}
           alt="gun_n_roses"
           className="w-full h-[700px] object-cover blur-md z-0"
         />
       </div>
-      <Navbar />
       <div className="justify-center col-start-2 col-span-10 -mt-[550px] z-10">
         <div className="grid grid-cols-10 text-white">
           <span className="col-span-6 text-[96px] font-semibold">
-            Guns N’ Rose Live in Bangkok
+            {eventDetailDummy.title}
           </span>
           <div className="col-end-10 bg-white w-1"></div>
-          <span className="col-end-11 text-right self-end">ParcParagon</span>
+          <span className="col-end-11 text-right self-end font-semibold">
+            {eventDetailDummy.location}
+          </span>
         </div>
         <div className="grid-rows-2 mt-[60px]">
           <Image
-            src="https://scontent.fbkk31-1.fna.fbcdn.net/v/t39.30808-6/294815604_5222659704488338_2909628041657591105_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=e25c_dmcgSMAX9_vYy9&_nc_ht=scontent.fbkk31-1.fna&oh=00_AfBxu8VuB57M4voGQe3UZkOH1zN6zeIXCm7Sm5AmpFt-cA&oe=63A6CFB7"
+            src={eventDetailDummy.coverImg}
             width={1600}
             height={300}
             alt="gun_n_roses"
@@ -33,18 +56,19 @@ function Detail() {
           />
           <div className="grid grid-cols-10 gap-4 mt-16 text-white">
             <div className="bg-[#1D1D1D] rounded-lg col-span-6 w-full h-20 flex items-center justify-center flex-col">
-              <p>Guns N’ Roses Live in Bangkok</p>
-              <p>9 November 2022</p>
+              <p className="font-semibold font-lg">{eventDetailDummy.title}</p>
             </div>
             <div className="bg-[#1D1D1D] rounded-lg col-span-4 w-full h-20 flex items-center justify-center flex-col">
-              <p>Wed 9 November 2022, 20:00</p>
-              <p>Thunderdome Stadium</p>
+              <p className="font-semibold">
+                {eventDetailDummy.date}, {eventDetailDummy.time}
+              </p>
+              <p>{eventDetailDummy.location}</p>
             </div>
           </div>
           <div className="grid grid-cols-10 gap-4 mt-16">
             <div className="col-span-5">
               <Image
-                src="https://scontent.fbkk31-1.fna.fbcdn.net/v/t39.30808-6/294811374_5222694421151533_429907347781770456_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=dbeb18&_nc_ohc=FyVKddMZUZEAX8u7KT3&_nc_ht=scontent.fbkk31-1.fna&oh=00_AfCDqp7RMDcNJ6_Re_Dh09DU1x5x1WKflJrnHpCEdJYxBw&oe=63A83DAC"
+                src={eventDetailDummy.img}
                 width={1600}
                 height={300}
                 alt="gun_n_roses"
@@ -53,18 +77,60 @@ function Detail() {
             </div>
             <div className="col-span-5 pl-5">
               <p className="font-semibold text-[24px]">Event Description</p>
-              <p className="mt-5 text-[24px]">
-                Age Restrictions Admission to reserved seats for 6 years old and
-                above only. Admission to standing pen for 12 years old and above
-                only. Nightrain Member Pre-sale: 26 July 2022, 11 am - 11pm (GMT
-                +7) Public Sale: 28 July 11 am (GMT +7) onwards *Subscribe to
-                the IMC Live Global Newsletter by 26 July 2022, 12 pm (GMT+7)
-                via http://bit.ly/IMCLiveFans⁣ to access the pre-sale. A
-                pre-sale link will be sent to you via email on 26 July 2022, 6
-                pm (GMT +7).
-              </p>
+              <p className="mt-5 text-lg">{eventDetailDummy.description}</p>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="col-start-2 col-end-12 my-10 bg-[#1E1E1E] rounded-md w-full">
+        <div className="text-white p-5 w-full grid grid-cols-9">
+          <div className="col-span-3 font-semibold text-lg">
+            <p>Ticket Type</p>
+          </div>
+          <div className="col-span-3 text-end font-semibold text-lg">
+            <p>Price</p>
+          </div>
+          <div className="col-span-3 text-end font-semibold text-lg">
+            <p>Qty.</p>
+          </div>
+          <hr className="col-span-9 my-4"></hr>
+          {eventDetailDummy.ticket.map((item: TicketType, i: number) => (
+            <div
+              className="flex justify-between items-start col-span-9 grid grid-cols-9 my-2"
+              key={i}
+            >
+              <div className="col-span-3 font-medium">
+                <p>{item.title}</p>
+              </div>
+              <div className="col-span-3 text-end font-medium">
+                <p>{item.price}</p>
+              </div>
+              <div className="col-span-3 text-end font-medium">
+                <Menu>
+                  <Menu.Button className="border border-white py-2 px-3 rounded">
+                    <div className="flex gap-x-2 items-center">
+                      <p>Select</p>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-4 h-4"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                  </Menu.Button>
+                  {ticketNumber.map((item: number, i: number) => (
+                    <Menu.Items key={i}>{item}</Menu.Items>
+                  ))}
+                </Menu>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>

b0356371a8d17785aa084b7e2b86fc127e4f9fd7
nata-non <63070045@kmitl.ac.th>
2022-12-22 01:42:07 +0700

    feat : hide and show password

--- packages/web/pages/sign-up.tsx
+++ packages/web/pages/sign-up.tsx
@@ -8,7 +8,14 @@ function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [confirmPasswordShown, setConfirmPasswordShown] = useState(false);
+  function togglePassword() {
+    setPasswordShown(!passwordShown);
+  }
+  function toggleConfirmPassword() {
+    setConfirmPasswordShown(!confirmPasswordShown);
+  }
   return (
     <div className="flex flex-col justify-center items-center my-20">
       <div className="w-auto py-10 rounded-md overflow-hidden shadow-lg px-10 bg-white">
@@ -85,25 +92,111 @@ function Signup() {
             <label htmlFor="" className="text-gray-800 font-semibold">
               Password
             </label>
-            <input
-              className="border-2 w-full py-2 px-2 rounded-md"
-              placeholder="Password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            ></input>
+            <div className="relative">
+              <div className="absolute top-1 right-1 flex items-center">
+                {passwordShown ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-9 h-9 cursor-pointer"
+                    onClick={togglePassword}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-9 h-9 cursor-pointer"
+                    onClick={togglePassword}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                )}
+              </div>
+              <input
+                className="border-2 w-full py-2 px-2 rounded-md"
+                placeholder="Password"
+                type={passwordShown ? 'text' : 'password'}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              ></input>
+            </div>
           </div>
           <div>
             <label htmlFor="" className="text-gray-800 font-semibold">
               Confirm Password
             </label>
-            <input
-              className="border-2 w-full py-2 px-2 rounded-md"
-              placeholder="Confirm Password"
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
-            ></input>
+            <div className="relative">
+              <div className="absolute top-1 right-1 flex items-center">
+                {confirmPasswordShown ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-9 h-9 cursor-pointer"
+                    onClick={toggleConfirmPassword}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-9 h-9 cursor-pointer"
+                    onClick={toggleConfirmPassword}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                )}
+              </div>
+              <input
+                className="border-2 w-full py-2 px-2 rounded-md"
+                placeholder="Confirm Password"
+                type={confirmPasswordShown ? 'text' : 'password'}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+              ></input>
+            </div>
           </div>
         </div>
         <div className="flex justify-center px-6 pb-2 items-center">

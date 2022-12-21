cc9812aa9d34d1d77a762064e63afd32c54c5da7
santhitak <63070175@kmitl.ac.th>
2022-12-21 12:41:11 +0700

    feat: get ticket transition on hover

--- packages/web/components/EventCard.tsx
+++ packages/web/components/EventCard.tsx
@@ -10,37 +10,53 @@ const EventCard = () => {
   return (
     <>
       <div className="flex justify-between">
-        {mockData.map((item: data, i: number) => {
-          return (
-            <div className="my-6 flex flex-col gap-y-2 w-[230px]" key={i}>
-              <div className="bg-black w-[230px] h-[320px] rounded-md"></div>
-              <p className="text-gray-400 text-sm">{item.date}</p>
-              <p className="font-semibold text-lg">{item.title}</p>
-              <div className="flex gap-x-1 items-center text-gray-400 text-sm">
+        {mockData.map((item: data, i: number) => (
+          <div className="my-6 flex flex-col gap-y-2 w-[230px]" key={i}>
+            <div className="bg-gray-600 w-[230px] h-[320px] rounded-md">
+              <div className="w-full h-full items-center justify-center flex gap-x-2 rounded-md transition-all hover:bg-black/30 opacity-0 hover:opacity-100 ">
+                <p className="text-white text-xl font-semibold">GET TICKET</p>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-4 h-4"
+                  stroke="white"
+                  className="w-6 h-6"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
                   />
                 </svg>
-                <p>{item.location}</p>
               </div>
             </div>
-          );
-        })}
+            <p className="text-gray-400 text-sm">{item.date}</p>
+            <p className="font-semibold text-lg">{item.title}</p>
+            <div className="flex gap-x-1 items-center text-gray-400 text-sm">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                />
+              </svg>
+              <p>{item.location}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
